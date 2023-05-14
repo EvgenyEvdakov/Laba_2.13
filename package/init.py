b5db1459eac3b8a42219ae48092ca84512bed8a0
Evgeny <kkrutkov02@gmail.com>
2023-05-14 12:34:01 +0300

    init

--- package/init.py
+++ package/init.py
@@ -1,3 +1,5 @@
-from .routes import Route, input_routes, find_route
+from .add_route import add_route
+from .sort_routes import sort_routes
+from .find_route import find_route
 
-__all__ = ["Route", "input_routes", "find_route"]
+__all__ = ["add_route", "sort_routes", "find_route"]
